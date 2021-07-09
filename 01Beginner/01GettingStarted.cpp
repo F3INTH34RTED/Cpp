2e3a0eb953e3e284a265d78207c7a1901b345e1e
F3INT H34RTED <68115056+F3INTH34RTED@users.noreply.github.com>
2021-07-09 15:09:18 +0400

    Updated chapter 1 code

--- 01Beginner/01GettingStarted.cpp
+++ 01Beginner/01GettingStarted.cpp
@@ -5,13 +5,8 @@ using std::cin;
 
 int main()
 {
-    int x;
-    cout << "Enter a number between 1 - 10: ";
-    cin >> x;
-    if (x < 1 || x > 10) exit(0);
-    for (int i = 0; i < x; ++i) 
-    {
-        cout << i << '\n';
-    }
+    int x = 2, y = 4, z;
+    z = x + y;
+    cout << z << '\n';
     return 0;
 }
