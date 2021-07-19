7d23b814f5d2d58a650159a50402e674f558f41d
F3INT H34RTED <68115056+F3INTH34RTED@users.noreply.github.com>
2021-07-20 00:12:20 +0400

    added progress tracker file and chapter 3 exercises file

---
+++ 01Beginner/C03StringsVectorsAndArrays/03StringsVectorsAndArrays.cpp
@@ -0,0 +1,372 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+void Exercise01();
+void Exercise02();
+void Exercise03();
+void Exercise04();
+void Exercise05();
+void Exercise06();
+void Exercise07();
+void Exercise08();
+void Exercise09();
+void Exercise10();
+void Exercise11();
+void Exercise12();
+void Exercise13();
+void Exercise14();
+void Exercise15();
+void Exercise16();
+void Exercise17();
+void Exercise18();
+void Exercise19();
+void Exercise20();
+void Exercise21();
+void Exercise22();
+void Exercise23();
+void Exercise24();
+void Exercise25();
+void Exercise26();
+void Exercise27();
+void Exercise28();
+void Exercise29();
+void Exercise30();
+void Exercise31();
+void Exercise32();
+void Exercise33();
+void Exercise34();
+void Exercise35();
+void Exercise36();
+void Exercise37();
+void Exercise38();
+void Exercise39();
+void Exercise40();
+void Exercise41();
+void Exercise42();
+void Exercise43();
+void Exercise44();
+void Exercise45();
+
+int main()
+{
+    // Exercise01();
+    // Exercise02();
+    // Exercise03();
+    // Exercise04();
+    // Exercise05();
+    // Exercise06();
+    // Exercise07();
+    // Exercise08();
+    // Exercise09();
+    // Exercise10();
+    // Exercise11();
+    // Exercise12();
+    // Exercise13();
+    // Exercise14();
+    // Exercise15();
+    // Exercise16();
+    // Exercise17();
+    // Exercise18();
+    // Exercise19();
+    // Exercise20();
+    // Exercise21();
+    // Exercise22();
+    // Exercise23();
+    // Exercise24();
+    // Exercise25();
+    // Exercise26();
+    // Exercise27();
+    // Exercise28();
+    // Exercise29();
+    // Exercise30();
+    // Exercise31();
+    // Exercise32();
+    // Exercise33();
+    // Exercise34();
+    // Exercise35();
+    // Exercise36();
+    // Exercise37();
+    // Exercise38();
+    // Exercise39();
+    // Exercise40();
+    // Exercise41();
+    // Exercise42();
+    // Exercise43();
+    // Exercise44();
+    // Exercise45();
+    
+    return 0;
+}
+
+//
+void Exercise01()
+{
+
+}
+
+//
+void Exercise02()
+{
+
+}
+
+//
+void Exercise03()
+{
+
+}
+
+//
+void Exercise04()
+{
+
+}
+
+//
+void Exercise05()
+{
+
+}
+
+//
+void Exercise06()
+{
+
+}
+
+//
+void Exercise07()
+{
+
+}
+
+//
+void Exercise08()
+{
+
+}
+
+//
+void Exercise09()
+{
+
+}
+
+//
+void Exercise10()
+{
+
+}
+
+//
+void Exercise11()
+{
+
+}
+
+//
+void Exercise12()
+{
+
+}
+
+//
+void Exercise13()
+{
+
+}
+
+//
+void Exercise14()
+{
+
+}
+
+//
+void Exercise15()
+{
+
+}
+
+//
+void Exercise16()
+{
+
+}
+
+//
+void Exercise17()
+{
+
+}
+
+//
+void Exercise18()
+{
+
+}
+
+//
+void Exercise19()
+{
+
+}
+
+//
+void Exercise20()
+{
+
+}
+
+//
+void Exercise21()
+{
+
+}
+
+//
+void Exercise22()
+{
+
+}
+
+//
+void Exercise23()
+{
+
+}
+
+//
+void Exercise24()
+{
+
+}
+
+//
+void Exercise25()
+{
+
+}
+
+//
+void Exercise26()
+{
+
+}
+
+//
+void Exercise27()
+{
+
+}
+
+//
+void Exercise28()
+{
+
+}
+
+//
+void Exercise29()
+{
+
+}
+
+//
+void Exercise30()
+{
+
+}
+
+//
+void Exercise31()
+{
+
+}
+
+//
+void Exercise32()
+{
+
+}
+
+//
+void Exercise33()
+{
+
+}
+
+//
+void Exercise34()
+{
+
+}
+
+//
+void Exercise35()
+{
+
+}
+
+//
+void Exercise36()
+{
+
+}
+
+//
+void Exercise37()
+{
+
+}
+
+//
+void Exercise38()
+{
+
+}
+
+//
+void Exercise39()
+{
+
+}
+
+//
+void Exercise40()
+{
+
+}
+
+//
+void Exercise41()
+{
+
+}
+
+//
+void Exercise42()
+{
+
+}
+
+//
+void Exercise43()
+{
+
+}
+
+//
+void Exercise44()
+{
+
+}
+
+//
+void Exercise45()
+{
+
+}
+
